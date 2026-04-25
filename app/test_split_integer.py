bc97d655471f35648d118db1c4d259488718bc0c
Tymur <mr.kanis82@gmail.com>
2026-04-25 10:52:41 -0400

    hope for done

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -16,7 +16,7 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     result = split_integer(5, 3)
-    assert result == [2, 2, 1]
+    assert result == [1, 2, 2]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
